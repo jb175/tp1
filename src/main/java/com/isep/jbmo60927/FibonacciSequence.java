38d9a0cf14e2a6d55d22f26393604334339218ba
jb <jb.moufle@gmail.com>
2023-02-21 16:58:15 +0100

    comments

--- src/main/java/com/isep/jbmo60927/FibonacciSequence.java
+++ src/main/java/com/isep/jbmo60927/FibonacciSequence.java
@@ -1,16 +1,27 @@
 package com.isep.jbmo60927;
 import java.util.Scanner;
 
+
 public class FibonacciSequence {
+
+  //we don't use the constructor
   public FibonacciSequence() {
     throw new IllegalAccessError("Utility class");
   }
 
+  //two differnet methods to resolve this problem
   public enum Methods {
     RECURSIVE,
     ITERATIVE
   }
 
+  
+  /** 
+   * call the 2 differents methods
+   * @param recursive the method used to find the result
+   * @param n the max number of the Fibonacci Sequence
+   * @return int the value return by this sequence
+   */
   public static int fib(Methods recursive, int n) {
     if (n==0 || n==1)
       return n;
@@ -25,6 +36,12 @@ public class FibonacciSequence {
       }
   }
 
+  
+  /** 
+   * recursive methode to resolve this problem
+   * @param n the max number of the Fibonacci Sequence
+   * @return int the value return by this sequence
+   */
   private static int recursiveFib(int n){
     if(n==0|| n==1)
       return n;
@@ -32,25 +49,37 @@ public class FibonacciSequence {
       return recursiveFib(n-1)+recursiveFib(n-2);
   }
 
+  
+  /** 
+   * iterative methode to resolve this problem
+   * @param n the max number of the Fibonacci Sequence
+   * @return int the value return by this sequence
+   */
   private static int iterativeFib(int n){
-    if(n==0||n==1)
+    if(n==0)
       return n;
     else {
-      int counter = 0;
-      for(int i=1; i< (n-2); i++) {
-        if (i == 0 || i == (n-1))
-          counter += i; //we add the first and the second to last element because each of them are added only one time
-        else
-          counter += (2*i); //and 2 times for evry other element
+      int first = 0;
+      int second = 1;
+      int temp = 0;
+      for (int i = 1; i < n; i++) {
+        temp = first + second;
+        first = second;
+        second = temp;
       }
-      return counter;
+      return second;
     }
   }
 
 
+  
+  /** 
+   * main function to start the algorithm
+   * @param args[]
+   */
   public static void main(String args[]){
     Scanner sc= new Scanner(System.in);
-    System.out.println("Enter a number:");
+    System.out.print("Enter a number: ");
     int n=sc.nextInt();
     sc.close();
     System.out.println(String.format("Recursive: %d", FibonacciSequence.fib(Methods.RECURSIVE, n)));
